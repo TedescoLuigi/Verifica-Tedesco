a714bcc2d5fcd87e80f53f2fd7794dd808d9caa0
tedesco.luigi <tedesco.luigi@INF-1018>
2026-02-25 13:30:53 +0100

    fine

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿using System;
+using System.Diagnostics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Verifica_Tedesco
 {
@@ -17,32 +19,89 @@ namespace Verifica_Tedesco
                     if (parola[i] == parola[j])
                     {
                         Console.WriteLine("La stringa contiene lettere duplicate.");
-                        return false;
+                        return true;
                     }
                 }
             }
             Console.WriteLine("La stringa contiene lettere diverse.");
-            return true;
+            return false;
         }
 
 
         //Esercizio B
-      //Progettare e implementare una funzione che consenta di generare
-     //una nuova stringa costituita esclusivamente dalle lettere diverse della seguente stringa
+        //Progettare e implementare una funzione che consenta di generare
+        //una nuova stringa costituita esclusivamente dalle lettere diverse della seguente stringa
 
-        static string Genera(string parola) 
-        { 
-          
-        
-        }
-        static void Main(string[] args)
+
+        static string Genera(string frase)
         {
-            string parola;
-            Console.WriteLine("Inserisci una parola:");
-             parola = Console.ReadLine();
+            //  Rimuovo gli spazi con un for
+            string fraseSenzaSpazi = "";
 
-            Console.WriteLine(verifica(parola));
-            
+            for (int i = 0; i < frase.Length; i++)
+            {
+                if (frase[i] != ' ')
+                {
+                    fraseSenzaSpazi = fraseSenzaSpazi + frase[i];
+                }
+            }
+
+            frase = fraseSenzaSpazi;
+
+            string nuovastringa;
+            for (int i = 0; i < frase.Length; i++)
+            {
+                if(nuovastringa.IndexOf(frase[i]) == -1)
+                {
+                    nuovastringa = nuovastringa + frase[i];
+                }
+
+            }
+
+            //Esercizio C
+            //E’ data una stringa contenente 20 cifre che rappresentano i lanci di un dado
+            //Progettare e implementare un algoritmo che consenta di generare e visualizzare un
+            //array di interi che contenga nella posizione 0 il numero di volte che è uscito 1, nella posizione 1
+            //il numero di volte che è uscito 2 e così via fino alla posizione 5 che contiene il numero di volte che è uscito 6.
+
+
+            static int[] ContaDadi(string lanci)
+            {
+                int[] contatore = new int[6];
+                for (int i = 0; i < lanci.Length; i++)
+                {
+                    int dado = lanci[i];
+                    if (dado >= 1 && dado <= 6)
+                    {
+                        int c = dado - 1;
+                        contatore[c]++;
+                    }
+                }
+                return contatore;
+            }
+            static void Main(string[] args)
+            {
+
+                //es1
+                string parola;
+                Console.WriteLine("Inserisci una parola:");
+                parola = Console.ReadLine();
+
+                Console.WriteLine(verifica(parola));
+
+
+
+                //es2
+
+                Console.WriteLine("inserisci una parola");
+                string frase = Console.ReadLine();
+                Console.WriteLine(Genera(frase));
+
+                //es3
+
+                Console.WriteLine(ContaDadi);
+
+            }
         }
     }
 }
